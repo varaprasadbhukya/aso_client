1adb8cc91cbae80cbce84092243b02c502b39226
unknown <varaprasad.bhavya@gmail.com>
2024-02-16 00:15:14 +0530

    added search app functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import './App.css';
-import Appdata from './Appdata';
-import Home from './Home';
-import SearchApp from './SearchApp';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Integrations from './Integrations';
-import Reviews from './Reviews';
-import Ratings from './Ratings';
-import Registration from './components/registration/Registration';
-import OrgForm from './components/registration/OrgForm';
+import "./App.css";
+import Appdata from "./Appdata";
+import Home from "./Home";
+import SearchApp from "./SearchApp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Integrations from "./Integrations";
+import Reviews from "./Reviews";
+import Ratings from "./Ratings";
+import Registration from "./components/registration/Registration";
+import OrgForm from "./components/registration/OrgForm";
+import RegSuccess from "./components/registration/RegSuccess";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={< Home />}></Route>
-          <Route exact path='/signup' element={< Registration />}></Route>
-          <Route exact path='/about-org' element={< OrgForm />}></Route>
-          <Route exact path='/search' element={< SearchApp />}></Route>
-          <Route exact path='/appdata' element={< Appdata />}></Route>
-          <Route exact path='/integrations' element={< Integrations />}></Route>
-          <Route exact path='/reviews' element={< Reviews />}></Route>
-          <Route exact path='/ratings' element={< Ratings />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/signup" element={<Registration />}></Route>
+          <Route exact path="/about-org" element={<OrgForm />}></Route>
+          <Route exact path="/reg_success" element={<RegSuccess />}></Route>
+          <Route exact path="/search" element={<SearchApp />}></Route>
+          <Route exact path="/appdata" element={<Appdata />}></Route>
+          <Route exact path="/integrations" element={<Integrations />}></Route>
+          <Route exact path="/reviews" element={<Reviews />}></Route>
+          <Route exact path="/ratings" element={<Ratings />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/registration/OrgForm.js
+++ src/components/registration/OrgForm.js
@@ -1,97 +1,161 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 const OrgForm = () => {
-    const [step, setStep] = useState(1);
-    const [fullName, setFullName] = useState('');
-    const [companyName, setCompanyName] = useState('');
-    const [jobFunction, setJobFunction] = useState('');
-    const [country, setCountry] = useState('');
-    const [appName, setAppName] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (step === 1) {
-        setStep(2);
-      } else {
-        // Handle submission for step 2
-        console.log('Form submitted:', {
-          fullName,
-          companyName,
-          jobFunction,
-          country,
-          appName,
+  const navigate = useNavigate();
+  const [step, setStep] = useState(1);
+  const [fullName, setFullName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [jobFunction, setJobFunction] = useState("");
+  const [country, setCountry] = useState("");
+  const [appName, setAppName] = useState("");
+  const [data, setData] = useState("");
+  const [appId, setAppId] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (step === 1) {
+      setStep(2);
+    } else {
+      // Handle submission for step 2
+      console.log("Form submitted:", {
+        fullName,
+        companyName,
+        jobFunction,
+        country,
+        appName,
+      });
+      try {
+        let res = await api({
+          url: "/",
+          method: "POST",
+          responseType: "json",
+          data: {
+            fullName,
+            companyName,
+            jobFunction,
+            country,
+            appName,
+            appId,
+          },
         });
-        // Reset form fields
-        setFullName('');
-        setCompanyName('');
-        setJobFunction('');
-        setCountry('');
-        setAppName('');
-        // Go back to step 1
-        setStep(1);
+        if (res) {
+          setFullName("");
+          setCompanyName("");
+          setJobFunction("");
+          setCountry("");
+          setAppName("");
+          navigate("/reg_success");
+        }
+      } catch (error) {
+        if (error.response.status === 401) navigate("/");
       }
-    };
-  
-    return (
-      <div className="two-step-form-container">
-        <form onSubmit={handleSubmit}>
-          {step === 1 && (
-            <>
-              <label htmlFor="fullName">Full Name:</label>
-              <input
-                type="text"
-                id="fullName"
-                placeholder="Full Name"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                required
-              />
-              <label htmlFor="companyName">Company Name:</label>
-              <input
-                type="text"
-                id="companyName"
-                placeholder="Company Name"
-                value={companyName}
-                onChange={(e) => setCompanyName(e.target.value)}
-                required
-              />
-              <label htmlFor="jobFunction">Job Function (optional):</label>
-              <input
-                type="text"
-                id="jobFunction"
-                placeholder="Job Function"
-                value={jobFunction}
-                onChange={(e) => setJobFunction(e.target.value)}
-              />
-              <button type="submit">Save and Proceed</button>
-            </>
-          )}
-  
-          {step === 2 && (
-            <>
-              <label htmlFor="country">Select your country:</label>
-              <select value={country} onChange={(e) => setCountry(e.target.value)} required>
-                <option value="">Select your country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                {/* Add more country options here */}
-              </select>
-              <label htmlFor="appName">Search for your app:</label>
-              <input
-                type="text"
-                id="appName"
-                placeholder="Search for your app"
-                value={appName}
-                onChange={(e) => setAppName(e.target.value)}
-                required
-              />
-              <button type="submit">Save and Proceed</button>
-            </>
-          )}
-        </form> 
-      </div>
-    );
+    }
   };
-  
-  export default OrgForm;
+
+  const searchApp = async (appname) => {
+    if (appname.length > 3) {
+      try {
+        let res = await api({
+          url: "/search_app",
+          method: "POST",
+          responseType: "json",
+          data: {
+            appname,
+          },
+        });
+        if (res) {
+          setData(res.data.data);
+        }
+      } catch (error) {
+        if (error.response.status === 401) navigate("/");
+      }
+    }
+  };
+
+  return (
+    <div className="two-step-form-container">
+      <form onSubmit={handleSubmit}>
+        {step === 1 && (
+          <>
+            <label htmlFor="fullName">Full Name:</label>
+            <input
+              type="text"
+              id="fullName"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+            <label htmlFor="companyName">Company Name:</label>
+            <input
+              type="text"
+              id="companyName"
+              placeholder="Company Name"
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+              required
+            />
+            <label htmlFor="jobFunction">Job Function (optional):</label>
+            <input
+              type="text"
+              id="jobFunction"
+              placeholder="Job Function"
+              value={jobFunction}
+              onChange={(e) => setJobFunction(e.target.value)}
+            />
+            <button type="submit">Save and Proceed</button>
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+            <label htmlFor="country">Select your country:</label>
+            <select
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            >
+              <option value="">Select your country</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              {/* Add more country options here */}
+            </select>
+            {console.log(appName, "----------appname")}
+            <label htmlFor="appName">Search for your app:</label>
+
+            <input
+              type="text"
+              id="appName"
+              placeholder="Search for your app"
+              value={appName}
+              onChange={(e) => {
+                setAppName(e.target.value);
+                searchApp(e.target.value);
+              }}
+              required
+            />
+            {data &&
+              data.map((item) => (
+                <div className="search-inner">
+                  <img
+                    className="icon"
+                    src={item.icon}
+                    alt="icon"
+                    onClick={() => {
+                      setAppName(item.title);
+                      setAppId(item.appId);
+                    }}
+                  />
+                </div>
+              ))}
+            <button type="submit">submit</button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default OrgForm;
